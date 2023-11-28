25d666dc952d3f8a6832e34b6f63bbdbaf271670
Santiago Olivos <santiago.olivos@uc.cl>
2023-11-28 10:57:58 -0300

    feat/functions

--- challenge/model.py
+++ challenge/model.py
@@ -1,58 +1,205 @@
-import pandas as pd
+# import pandas as pd
 
-from typing import Tuple, Union, List
+# from typing import Tuple, Union, List
+
+# class DelayModel:
+
+#     def __init__(
+#         self
+#     ):
+#         self._model = None # Model should be saved in this attribute.
+
+#     def preprocess(
+#         self,
+#         data: pd.DataFrame,
+#         target_column: str = None
+#     ) -> Union(Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame):
+#         """
+#         Prepare raw data for training or predict.
+
+#         Args:
+#             data (pd.DataFrame): raw data.
+#             target_column (str, optional): if set, the target is returned.
+
+#         Returns:
+#             Tuple[pd.DataFrame, pd.DataFrame]: features and target.
+#             or
+#             pd.DataFrame: features.
+#         """
+#         return
+
+#     def fit(
+#         self,
+#         features: pd.DataFrame,
+#         target: pd.DataFrame
+#     ) -> None:
+#         """
+#         Fit model with preprocessed data.
+
+#         Args:
+#             features (pd.DataFrame): preprocessed data.
+#             target (pd.DataFrame): target.
+#         """
+#         return
+
+#     def predict(
+#         self,
+#         features: pd.DataFrame
+#     ) -> List[int]:
+#         """
+#         Predict delays for new flights.
+
+#         Args:
+#             features (pd.DataFrame): preprocessed data.
+        
+#         Returns:
+#             (List[int]): predicted targets.
+#         """
+#         return
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import xgboost as xgb
+from datetime import datetime
+import warnings
+
+warnings.filterwarnings('ignore')
+
+top_10_features = [
+    "OPERA_Latin American Wings", 
+    "MES_7",
+    "MES_10",
+    "OPERA_Grupo LATAM",
+    "MES_12",
+    "TIPOVUELO_I",
+    "MES_4",
+    "MES_11",
+    "OPERA_Sky Airline",
+    "OPERA_Copa Air"
+]
+
+# Funciones auxiliares fuera de la clase (puedes incluirlas dentro si lo prefieres)
+def get_period_day(date):
+    date_time = datetime.strptime(date, '%Y-%m-%d %H:%M:%S').time()
+    morning_min = datetime.strptime("05:00", '%H:%M').time()
+    morning_max = datetime.strptime("11:59", '%H:%M').time()
+    afternoon_min = datetime.strptime("12:00", '%H:%M').time()
+    afternoon_max = datetime.strptime("18:59", '%H:%M').time()
+    evening_min = datetime.strptime("19:00", '%H:%M').time()
+    evening_max = datetime.strptime("23:59", '%H:%M').time()
+    night_min = datetime.strptime("00:00", '%H:%M').time()
+    night_max = datetime.strptime("4:59", '%H:%M').time()
+    
+    if morning_min <= date_time <= morning_max:
+        return 'mañana'
+    elif afternoon_min <= date_time <= afternoon_max:
+        return 'tarde'
+    elif evening_min <= date_time <= evening_max or night_min <= date_time <= night_max:
+        return 'noche'
+
+def is_high_season(fecha):
+    fecha_año = int(fecha.split('-')[0])
+    fecha = datetime.strptime(fecha, '%Y-%m-%d %H:%M:%S')
+    range1_min = datetime.strptime('15-Dec', '%d-%b').replace(year = fecha_año)
+    range1_max = datetime.strptime('31-Dec', '%d-%b').replace(year = fecha_año)
+    range2_min = datetime.strptime('1-Jan', '%d-%b').replace(year = fecha_año)
+    range2_max = datetime.strptime('3-Mar', '%d-%b').replace(year = fecha_año)
+    range3_min = datetime.strptime('15-Jul', '%d-%b').replace(year = fecha_año)
+    range3_max = datetime.strptime('31-Jul', '%d-%b').replace(year = fecha_año)
+    range4_min = datetime.strptime('11-Sep', '%d-%b').replace(year = fecha_año)
+    range4_max = datetime.strptime('30-Sep', '%d-%b').replace(year = fecha_año)
+    
+    if ((fecha >= range1_min and fecha <= range1_max) or 
+        (fecha >= range2_min and fecha <= range2_max) or 
+        (fecha >= range3_min and fecha <= range3_max) or
+        (fecha >= range4_min and fecha <= range4_max)):
+        return 1
+    else:
+        return 0
+
+def get_min_diff(data):
+    fecha_o = datetime.strptime(data['Fecha-O'], '%Y-%m-%d %H:%M:%S')
+    fecha_i = datetime.strptime(data['Fecha-I'], '%Y-%m-%d %H:%M:%S')
+    min_diff = ((fecha_o - fecha_i).total_seconds())/60
+    return min_diff
 
 class DelayModel:
+    def __init__(self, model_type='logistic', balance_classes=False, top_features=None):
+        self.model_type = model_type
+        self.balance_classes = balance_classes
+        self.top_features = top_features
+        self._model = None
 
-    def __init__(
-        self
-    ):
-        self._model = None # Model should be saved in this attribute.
-
-    def preprocess(
-        self,
-        data: pd.DataFrame,
-        target_column: str = None
-    ) -> Union(Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame):
-        """
-        Prepare raw data for training or predict.
-
-        Args:
-            data (pd.DataFrame): raw data.
-            target_column (str, optional): if set, the target is returned.
-
-        Returns:
-            Tuple[pd.DataFrame, pd.DataFrame]: features and target.
-            or
-            pd.DataFrame: features.
-        """
-        return
-
-    def fit(
-        self,
-        features: pd.DataFrame,
-        target: pd.DataFrame
-    ) -> None:
-        """
-        Fit model with preprocessed data.
-
-        Args:
-            features (pd.DataFrame): preprocessed data.
-            target (pd.DataFrame): target.
-        """
-        return
-
-    def predict(
-        self,
-        features: pd.DataFrame
-    ) -> List[int]:
-        """
-        Predict delays for new flights.
-
-        Args:
-            features (pd.DataFrame): preprocessed data.
+    def preprocess(self, data: pd.DataFrame):
+        # Asumiendo que ya tienes definidas las funciones `get_period_day`, `is_high_season`, y `get_min_diff`
+        data['period_day'] = data['Fecha-I'].apply(get_period_day)
+        data['high_season'] = data['Fecha-I'].apply(is_high_season)
+        data['min_diff'] = data.apply(get_min_diff, axis=1)
+        data['delay'] = np.where(data['min_diff'] > 15, 1, 0)
         
-        Returns:
-            (List[int]): predicted targets.
-        """
-        return
+        data = pd.get_dummies(data, columns=['OPERA', 'MES', 'TIPOVUELO'], prefix=['OPERA', 'MES', 'TIPOVUELO'])
+        if self.top_features:
+            # Agrega una verificación para asegurarte de que las columnas existen
+            missing_cols = [col for col in self.top_features if col not in data.columns]
+            if missing_cols:
+                raise ValueError(f"Las siguientes columnas no están presentes en el DataFrame: {missing_cols}")
+            data = data[self.top_features + ['delay']]
+
+        return data
+
+    def fit(self, features: pd.DataFrame, target: pd.DataFrame):
+        # Calcular las proporciones de las clases
+        prop_class_0 = len(target[target == 0]) / len(target)
+        prop_class_1 = len(target[target == 1]) / len(target)
+        class_weight = {0: prop_class_0, 1: prop_class_1} if self.balance_classes else None
+
+        # Inicializar el modelo
+        if self.model_type == 'logistic':
+            self._model = LogisticRegression(class_weight=class_weight)
+        elif self.model_type == 'xgboost':
+            scale_pos_weight = prop_class_0 / prop_class_1 if self.balance_classes else 1
+            self._model = xgb.XGBClassifier(random_state=1, learning_rate=0.01, scale_pos_weight=scale_pos_weight)
+
+        self._model.fit(features, target)
+
+    def predict(self, features: pd.DataFrame) -> np.array:
+        
+        predictions = self._model.predict(features)
+        
+        # Preparar los resultados para la salida de la API
+        results = []
+        for index, prediction in enumerate(predictions):
+            result = {
+                'FlightIndex': features.iloc[index].name,  # O alguna otra identificación única del vuelo
+                'Prediction': int(prediction)  # Convertir a int por si acaso el modelo devuelve un tipo de dato diferente
+            }
+            results.append(result)
+        
+        print(results)
+        return results
+
+
+# Uso de la clase:
+data = pd.read_csv('./data/data.csv')
+
+# Crear una instancia de la clase DelayModel
+model = DelayModel(model_type='logistic', balance_classes=True, top_features=top_10_features)
+
+# Preprocesar los datos
+preprocessed_data = model.preprocess(data)
+
+# Dividir los datos en conjuntos de entrenamiento y prueba
+features = preprocessed_data.drop('delay', axis=1)
+target = preprocessed_data['delay']
+x_train, x_test, y_train, y_test = train_test_split(features, target, test_size=0.33, random_state=42)
+
+# Entrenar el modelo con los datos de entrenamiento
+model.fit(x_train, y_train)
+
+# Hacer predicciones con los datos de prueba
+predictions = model.predict(x_test)
